53be878e7f439c260427db29dc6d20f4ffef324c
Aaron Tadina <atadina@mail.greenriver.edu>
2020-04-27 16:41:01 -0400

    Food project initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once ("vendor/autoload.php");
+//instantiate the f3 Base class
+$f3 = Base::instance();
+
+//Default route
+$f3->route('GET /',function(){
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->run();
